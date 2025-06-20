f846cc92d3559555b13960e4397972c3a2e52bf6
HomerusJa <jakob@schluse.com>
2025-06-20 12:49:12 +0200

    📝 Add docstrings to types

--- src/qoi_py/types.py
+++ src/qoi_py/types.py
@@ -1,28 +1,38 @@
 import numpy as np
 import numpy.typing as npt
-
 from typing import Annotated
 from dataclasses import dataclass
 from enum import IntEnum
 
 
 class QOIColorspace(IntEnum):
+    """QOI colorspace indicator"""
+
     SRGB = 0
     LINEAR_RGB = 1
 
 
 class QOIChannelCount(IntEnum):
+    """QOI channel count"""
+
     RGB = 3
     RGBA = 4
 
 
-type RGBImageContent = Annotated[npt.NDArray[np.uint8], ("height", "width", 3)]
-type RGBAImageContent = Annotated[npt.NDArray[np.uint8], ("height", "width", 4)]
-type ImageContent = RGBAImageContent | RGBImageContent
+RGBImageContent = Annotated[npt.NDArray[np.uint8], ("height", "width", 3)]
+"""(height, width, 3) uint8 NumPy array for RGB image data."""
+
+RGBAImageContent = Annotated[npt.NDArray[np.uint8], ("height", "width", 4)]
+"""(height, width, 4) uint8 NumPy array for RGBA image data."""
+
+ImageContent = RGBAImageContent | RGBImageContent
+"""RGB or RGBA image data as a NumPy array."""
 
 
 @dataclass
 class RGBImage:
+    """RGB image with QOI colorspace and shape (height, width, 3)."""
+
     width: int
     height: int
     colorspace: QOIColorspace
@@ -31,6 +41,8 @@ class RGBImage:
 
 @dataclass
 class RGBAImage:
+    """RGBA image with QOI colorspace and shape (height, width, 4)."""
+
     width: int
     height: int
     colorspace: QOIColorspace
