02d478eca988001d970bdbbfb2576262c2c62929
HomerusJa <jakob@schluse.com>
2025-06-21 18:43:57 +0200

    ♻️ Refactor the two different opcode Enums into one
    This lets the from_byte method handle the significance logic, making the decoding logic simpler.

--- src/qoi_py/opcodes.py
+++ src/qoi_py/opcodes.py
@@ -2,25 +2,30 @@ from enum import IntEnum
 from typing import Self
 
 
-class QOI2BitOpcode(IntEnum):
-    """All two-bit opcodes in QOI format.
-
-    Only the first two bits are significant; the last six bits are used for data.
-    """
+class QOIOpcode(IntEnum):
+    """A QOI opcode."""
 
     INDEX = 0x00  # 00xxxxxx
     DIFF = 0x40  # 01xxxxxx
     LUMA = 0x80  # 10xxxxxx
     RUN = 0xC0  # 11xxxxxx
+    RGB = 0xFE  # 11111110
+    RGBA = 0xFF  # 11111111
 
     @classmethod
     def from_byte(cls, byte: int) -> Self:
-        """Get the opcode from a byte (0-255)."""
+        """Get the opcode from a byte (0-255).
+
+        As defined by the QOI specification, the 8-bit opcodes are tried first,
+        and if the byte does not match any of those, the first two bits are
+        masked to get the 2-bit opcodes.
+        """
         if not (0 <= byte < 256):
             raise ValueError("Byte must be in the range 0-255.")
-        return cls(byte & 0xC0)  # Mask to get the first two bits
 
+        # First try 8-bit opcodes as defined by the QOI specification
+        if byte in (cls.RGB, cls.RGBA):
+            return cls(byte)
 
-class QOIByteOpcode(IntEnum):
-    RGB = 0xFE  # 11111110
-    RGBA = 0xFF  # 11111111
+        # Now, mask to get the first two bits for 2-bit opcodes
+        return cls(byte & 0xC0)
